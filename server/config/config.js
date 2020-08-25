61ad529bf9fc0fae74f86927b34ed755285d91aa
cesar <cesargquiroga@gmail.com>
2020-08-25 03:13:49 +0200

    config

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 process.env.NODE_ENVDB = process.env.NODE_ENV === "dev"
     ? "mongodb://localhost:27017/cafe"
-    : "mongodb+srv://kaaru:GamJTIoPV1bkQ7AX@cluster0.8bg3t.mongodb.net/test";
+    : process.env.mongoURI;
